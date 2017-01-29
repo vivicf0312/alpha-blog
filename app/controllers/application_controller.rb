2dab6e21a19bc57cd9135b518fdadf45ae37a2c4
Viviana Bhandari <vbhandari@DB-MBP-Q1G3QD.local>
2017-01-29 11:58:35 -0800

    Complete authentication system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #make method available in the views
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user  ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    #create boolean
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
-      redirect_to users_path(user)
+      redirect_to user_path(user)
     else
       flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
@@ -15,6 +15,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
   end
 
 end
